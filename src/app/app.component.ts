a500c05fa206761407f3b1815e680267d4e88e40
Tanjin Habibur <tanjin.habibur@dbalab.it>
2019-11-04 17:39:27 +0100

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,33 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <h1>{{pageTitle}}</h1>
+    </div>
+  `,
+  
 })
+
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme product management';
 }
+
+
+
+
+
+
+
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'pm-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'Angular: Getting Started';
+// }
